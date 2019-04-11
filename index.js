d1d07d592089d1ded2f2e0e2b45b64938dfd6d4f
Yxwww <yuxi.wangcd@gmail.com>
2019-04-11 03:08:12 -0600

    CLD-1234 Add logic to write issueNumber to commit message

--- index.js
+++ index.js
@@ -2,19 +2,15 @@
 
 const { exec: childProcessExec } = require('child_process');
 const { promisify } = require('util');
-const { readFile: fsReadFile } = require('fs');
+const { readFile: fsReadFile, writeFile: fsWritefile } = require('fs');
 const { exit } = require('process');
 
 const exec = promisify(childProcessExec);
-const readFile = promisify(fsReadFile)
-
-const issueRegex = /(CLD-[^/]*)/g;
+const readFile = promisify(fsReadFile);
+const writeFile = promisify(fsWritefile);
 
 function complete() {
   exit(0);
-}
-function error() {
-  exit(1);
 }
 
 async function execStdout(cmd) {
@@ -29,21 +25,21 @@ async function getCurrentBranch() {
   return await execStdout('git symbolic-ref --short HEAD');
 }
 
-async function getCurrentIssueNumberFromBranchName() {
-  const currentBranch = await getCurrentBranch();
-  return currentBranch.match(issueRegex);
+async function getCurrentIssueNumberFromBranchName(issueRegex) {
+  return (await getCurrentBranch()).match(issueRegex);
 }
 
-async function checkCommitMessage(){
-  const message = (await readFile(process.argv[2], 'utf8')).trim();
-  const issueNumberMatchedStrs = await getCurrentIssueNumberFromBranchName();
+async function fillCommitMessageWithJiraIssueId(issueRegex){
+  const [,,commitMessagePath] = process.argv;
+  const issueNumberMatchedStrs = await getCurrentIssueNumberFromBranchName(issueRegex);
   if (!issueNumberMatchedStrs) {
-    console.log('It seems that you not working on a JIRA issue. Have you gone rogue ?');
     complete();
     return;
   }
   const [ issueNumber ] = issueNumberMatchedStrs;
+  const message = (await readFile(commitMessagePath, 'utf8')).trim();
+  console.log(issueNumber, message,);
+  await writeFile(commitMessagePath, ` ${issueNumber}\n${message}`, 'utf8')
 }
 
-checkCommitMessage();
-
+fillCommitMessageWithJiraIssueId(/(CLD-[^/]*)/g);
