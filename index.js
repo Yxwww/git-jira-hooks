8f222d7c8b2667d4dc76b3b398140f70557d381b
yxwww <yuxi.wangcd@gmail.com>
2019-04-10 23:33:55 -0600

    Add getCurrentBranch function
    - trying out promisified node apis in git hooks

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const { exec: childProcessExec } = require('child_process');
+const { promisify } = require('util');
+const { readFile: fsReadFile } = require('fs');
+const exec = promisify(childProcessExec);
+const readFile = promisify(fsReadFile)
+
+async function execStdout(cmd) {
+  const { stderr, stdout } = await exec(cmd);
+  if(stderr){
+    throw new Error(stderr);
+  }
+  return stdout.trim;
+}
+
+async function getCurrentBranch() {
+  return await execStdout('git symbolic-ref --short HEAD');
+}
+
+async function getCurrentIssueNumberFromBranchName() {
+  const currentBranch = await getCurrentBranch();
+  return currentBranch;
+}
+
+async function checkCommitMessage(){
+  const message = (await readFile(process.argv[2], 'utf8')).trim();
+  await getCurrentIssueNumberFromBranchName();
+  console.log(message);
+}
+
+checkCommitMessage();
+
