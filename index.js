fdfbb1b184e862cb2f56a5e7916268c27a79fcec
Yxwww <yuxi.wangcd@gmail.com>
2019-04-11 02:29:55 -0600

    Add condition handler if jira issue# not found

--- index.js
+++ index.js
@@ -3,10 +3,19 @@
 const { exec: childProcessExec } = require('child_process');
 const { promisify } = require('util');
 const { readFile: fsReadFile } = require('fs');
+const { exit } = require('process');
+
 const exec = promisify(childProcessExec);
 const readFile = promisify(fsReadFile)
 
 const issueRegex = /(CLD-[^/]*)/g;
+
+function complete() {
+  exit(0);
+}
+function error() {
+  exit(1);
+}
 
 async function execStdout(cmd) {
   const { stderr, stdout } = await exec(cmd);
@@ -27,8 +36,13 @@ async function getCurrentIssueNumberFromBranchName() {
 
 async function checkCommitMessage(){
   const message = (await readFile(process.argv[2], 'utf8')).trim();
-  const [ currentIssueNumber ] = await getCurrentIssueNumberFromBranchName();
-  console.log('currentIssue: ', currentIssueNumber);
+  const issueNumberMatchedStrs = await getCurrentIssueNumberFromBranchName();
+  if (!issueNumberMatchedStrs) {
+    console.log('It seems that you not working on a JIRA issue. Have you gone rogue ?');
+    complete();
+    return;
+  }
+  const [ issueNumber ] = issueNumberMatchedStrs;
 }
 
 checkCommitMessage();
